60614a0c1946a6589713f134464474b05fd19501
Nicolas Mérouze <nicolas.merouze@gmail.com>
2013-01-29 17:27:18 +0100

    Update to Netty 3.6.2

--- lib/katamari.rb
+++ lib/katamari.rb
@@ -1,7 +1,7 @@
 $:.unshift File.expand_path('../', __FILE__)
 require 'java'
-require File.expand_path('../../javalib/netty-3.5.11.Final.jar', __FILE__)
-require File.expand_path('../../target/katamari-0.1.1-SNAPSHOT.jar', __FILE__)
+require File.expand_path('../../javalib/netty-3.6.2.Final.jar', __FILE__)
+require File.expand_path('../../target/katamari-0.1.2-SNAPSHOT.jar', __FILE__)
 
 module Katamari
   module Handler
